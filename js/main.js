c99895c263be10fd6fb1c4161fd3572161539e83
AeroztaR <jorguinhos89@gmail.com>
2020-06-29 00:34:21 +0500

    Add progress-bar on scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Animation on scroll for cards
     AOS.init();
 
+    // Progress-bar on scroll
+    const progressBar = document.querySelector('.progress-bar');
+
+    window.addEventListener('scroll', () => {
+        
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        const winHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const percentage = winScroll / winHeight * 100;
+        
+        progressBar.style.width = percentage + '%';
+
+    });
 
 });
